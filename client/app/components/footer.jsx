a066fba54a5dd2097c8b151fadd333773b81b48d
shadowkudo <david.schildbock@heig-vd.ch>
2025-06-07 13:27:45 +0200

    fixed vertically center

--- client/app/components/footer.jsx
+++ client/app/components/footer.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import {CopyrightOutlined} from "@ant-design/icons";
-import {Link} from "react-router";
 
 export default function Footer() {
     return (
-        <footer className="bg-[#9172FF] text-white dark:text-black border-t border-gray-300 dark:border-gray-700 text-center p-4 text-sm">
-            <div className="flex items-center justify-center gap-1">
+        <footer className="bg-[#9172FF] text-white dark:text-black border-t border-gray-300 dark:border-gray-700 text-sm h-16 flex items-center justify-center">
+            <div className="flex items-center gap-1">
                 <strong>CryptoForge</strong>
-                <CopyrightOutlined />
+                <CopyrightOutlined className="text-base align-middle" />
                 <span>MIT License</span>
             </div>
         </footer>
